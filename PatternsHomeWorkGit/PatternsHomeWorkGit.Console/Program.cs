410d93beb117071338de13dcdb831dbe808c3415
EX2OUTCODING\mtorres <mtorres@outcoding.com>
2018-01-20 14:25:18 -0600

    Init commit - Spanish

---
+++ PatternsHomeWorkGit/PatternsHomeWorkGit.Console/Program.cs
@@ -0,0 +1,13 @@
+﻿using console = System.Console;
+
+namespace PatternsHomeWorkGit.Console
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            console.WriteLine("Hola mundo");
+            console.ReadKey();
+        }
+    }
+}
